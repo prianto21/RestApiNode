c462adc07eca2fa6222132052817adde212c14bb
prianto21 <rantoprianto21@gmail.com>
2021-01-15 11:23:14 +0700

    berhasil menampilkan semua data dan berdasarkan id

--- controller.js
+++ controller.js
@@ -4,5 +4,30 @@ var response=require('./res');
 var connection=require('./koneksi');
 
 exports.index=function(req,res){
-    response.ok("aplikasi rest api berjalan")
+    response.ok("aplikasi rest api berjalan",res)
+};
+
+//menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa=function(req,res){
+    connection.query('SELECT * FROM mahasiswa',function(error,rows,fields){
+        if(error){
+            console.log(error);
+        }else{
+            response.ok(rows,res)
+        }
+    });
+}
+
+//menampilkan semua data mahasiswa berdasarkan id
+exports.tampilberdasarkanid=function(req,res){
+    let id=req.params.id;
+    connection.query("SELECT * FROM mahasiswa WHERE id_mahasiswa = ?",[id],
+        function(error,rows,fields){
+            if(error){
+                console.log(error);
+            }else{
+                response.ok(rows,res)
+            }
+        }
+    );
 };
--- routes.js
+++ routes.js
@@ -2,6 +2,13 @@
 
 module.exports=function(app){
     var jsonku=require('./controller');
+    
     app.route('/')
     .get(jsonku.index);
+
+    app.route('/tampil')
+    .get(jsonku.tampilsemuamahasiswa);
+
+    app.route('/tampil/:id')
+    .get(jsonku.tampilberdasarkanid);
 }
