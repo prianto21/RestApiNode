7f73d52e83f2e257a60df2007b8a84264993e3ef
prianto21 <rantoprianto21@gmail.com>
2021-02-03 03:28:28 +0700

    memperbaiki bug daftar registerasi

--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,8 @@
-var express=require('express');
-var auth=require('./auth');
-var router=express.Router();
+var express = require("express");
+var auth = require("./auth");
+var router = express.Router();
 
 //daftarkan menu registrasi
-router.post('api/v1/register',auth.registrasi);
+router.post("/api/v1/register", auth.registrasi);
 
-module.exports=router;
+module.exports = router;
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var routes=require('./routes');
 routes(app);
 
 //daftarkan menu routes dari index
-app.use('/auth',require('./middleware'));
+app.use('/auth',require('./middleware/index'));
 
 app.listen(3000, () => {
     console.log(`Server started on port 3000`);
