7d11ae75ef2d9a27f07d84d291785881a045efa1
prianto21 <rantoprianto21@gmail.com>
2021-02-03 05:10:59 +0700

    menambahkan endpoint login dengan generate jwtoken

--- middleware/auth.js
+++ middleware/auth.js
@@ -15,8 +15,8 @@ exports.registrasi = function (req, res) {
     role: req.body.role,
     tanggal_daftar: new Date(),
   };
-
-  var query = "SELECT email FROM ?? WHERE ??";
+  //mengecek email apakah sudah ada atau belum
+  var query = "SELECT email FROM ?? WHERE ??=?";
   var table = ["user", "email", post.email];
   query = mysql.format(query, table);
 
@@ -36,8 +36,61 @@ exports.registrasi = function (req, res) {
           }
         });
       } else {
-        response.ok("email sudah terdaftar");
+        response.ok("email sudah terdaftar",res);
       }
     }
   });
 };
+
+//controller untuk login
+exports.login=function(req,res){
+    var post={
+        password: req.body.password,
+        email:req.body.email
+    }
+
+    var query="SELECT * FROM ?? WHERE ??=? AND ??=?";
+    var table=["user","password",md5(post.password), "email",post.email];
+
+    query=mysql.format(query,table);
+    connection.query(query,function(error,rows){
+        if(error){
+            console.log(error)
+        }else{
+            if(rows.length == 1 ){
+                var token=jwt.sign({rows},config.secret,{
+                    expiresIn:1440
+                });
+
+                id_user=rows[0].id;
+                var data={
+                    id_user:id_user,
+                    access_token:token,
+                    ip_address:ip.address()
+                }
+
+                var query="INSERT INTO ?? SET ?";
+                var table=["akses_token"];
+
+                query=mysql.format(query,table);
+                connection.query(query,data,function(error,rows){
+                    if(error){
+                        console.log(error);
+                    }else{
+                        res.json({
+                            success:true,
+                            message: 'Token berhasil di Generate',
+                            token:token,
+                            currUser:data.id_user
+                        });
+                    }
+                })
+            }else{
+                res.json({
+                    "ERROR" : true,
+                    "Message" : "Email atau Password Salah"
+                });
+            }
+        }
+    })
+}
